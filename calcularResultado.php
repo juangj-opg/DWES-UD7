41e4663ba6dc6f124c616bb715d335a59902ad53
JuanGJ <juangj1b@gmail.com>
2022-02-08 13:15:33 +0100

    Ejercicio 1 y 2 - Hecho sin pareja

---
+++ calcularResultado.php
@@ -0,0 +1,13 @@
+<?php //IMPORTANTE: ELIMINA EL ESPACIO ANTES DE LA INTERROGACIÓN
+// Instanciamos un nuevo servidor SOAP
+$uri="https://www.raulprietofernandez.net/images/blog/211/DNI/";
+$server = new SoapServer(null,array('uri'=>$uri));
+$server->addFunction("suma");
+$server->handle();
+
+// Función para obtener raíz cuadrada
+function suma($n1, $n2) {
+    $resultado=$n1 + $n2;
+    return $resultado;
+}
+?>
---
+++ ejercicio4.php
@@ -0,0 +1,13 @@
+<?php
+// MVC en Servicios
+
+/* Cliente : VC
+   - Formulario: View
+   - Procesado: Controller
+   - Llamada Servicio: Controller
+
+   Servicio: MC
+   - Acceso a ciudades: Model
+   - Procesamiento llamada: Controller
+*/
+?>
